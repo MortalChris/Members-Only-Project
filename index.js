c163db59f961f7e966d1b3b0e32813fd58127223
Christian Valeri <mortlaxris@outlook.com>
2023-11-03 16:13:49 -0500

    added if statemetn confirm password

--- index.js
+++ index.js
@@ -29,21 +29,29 @@ const UsersModel = mongoose.model('Users', new mongoose.Schema({
 }));
 
 
-        app.post("/sign-up.html", async (req, res, next) => {//Post needs to be the same as the file page location
-            try {
-                const users = new UsersModel({
-                    email: req.body.email,
-                    password: req.body.password
-                });
-                const result = await users.save();
-                res.redirect("log-in.html");
-                console.log(result);
-            } catch (err) {
-                console.log("Error");
+    app.post("/sign-up.html", async (req, res, next) => {//Post needs to be the same as the file page location
+        try {
+            if (req.body.password !== req.body.confirmPassword) {
+                // Password and Confirm Password do not match
+                    // const pErrorText = document.getElementById("signup-error-p");
+                    // pErrorText.textContent = "Password and Confirm Password do not match";
+                    console.log("Password and Confirm Password do not match");
                 res.redirect("sign-up.html");
-                return next(err);
-                };
-        });
+                return;
+            }
+            const users = new UsersModel({
+                email: req.body.email,
+                password: req.body.password
+            });
+            const result = await users.save();
+            res.redirect("log-in.html");
+            console.log(result);
+        } catch (err) {
+            console.log("Error");
+            res.redirect("sign-up.html");
+            return next(err);
+            };
+    });
 
 
 app.listen(port, () => {
